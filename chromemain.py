d11f8a7ec78dc08d0f570b04e4fdedb4db38b853
NEO <96645897+NEO-114514@users.noreply.github.com>
2025-03-24 16:32:36 +0800

    Delete chromemain.py

--- chromemain.py
+++
@@ -1,8 +0,0 @@
-import initial
-import webinitial
-import keylogic
-
-username, password, phonenumber, desired_time_slot = initial.getinfo()
-driver = webinitial.start(username,password)
-keylogic.make_reservation(driver,desired_time_slot,phonenumber)
-
